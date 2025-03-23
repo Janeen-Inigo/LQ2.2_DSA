ddfc5c45b09401ca8cbc8dd559cdfb596c45cb8b
Janeen-Inigo <inigojaneen01@gmail.com>
2025-03-24 07:45:21 +0800

    loop.js

--- loop.js
+++ loop.js
@@ -1,5 +1,5 @@
 let dog_breed = prompt("Enter the dog breed:");
-let print_count = parseInt(prompt("Enter how many times to print:"));
+let print_count = prompt("Enter how many times to print:");
 
 for (let i = 0; i < print_count; i++) {
     console.log(dog_breed);
